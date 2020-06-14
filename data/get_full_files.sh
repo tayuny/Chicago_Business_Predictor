c9076b93ddaacb619d4eefef830d3b7276174528
Ta-Yun Yang <tayuny@uchicago.edu>
2020-06-14 09:16:10 -0500

    Update get_full_files.sh

--- data/get_full_files.sh
+++ data/get_full_files.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-wget https://uchicago.box.com/shared/static/kc0jkv0m2e3cc9kbhuolcsnq5vo8sk8z.zip -O full_dataset.zip 
-
-unzip full_dataset.zip
-
-rm full_dataset.zip
+wget https://drive.google.com/file/d/1E7xSXy6Rn6Jd2APTPuGPufaNYGU4EycI/view?usp=sharing -O full_dataset.csv
